4e059c70c86f65aad1dbc0ceb24c1fcf049f1c2c
Uzmaa7 <uk9971khan@gmail.com>
2026-04-28 15:48:31 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./.env"
+})
+import app from "../Backend/app.js";
+import http from "http";
+
+
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
