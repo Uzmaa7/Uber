7d369f8cea8450a25c12e70db2eb098c37e6f9cf
Uzmaa7 <uk9971khan@gmail.com>
2026-04-29 21:11:57 +0530

    fix: correct captain validator sequence to prevent sanitizers from type-casting numbers to strings

--- Backend/validators/captain.Validator.js
+++ Backend/validators/captain.Validator.js
@@ -2,15 +2,20 @@ import {body} from "express-validator";
 
 const registerCaptainValidation = () => {
     return [
-        body("fullName.firstname")
-            .trim()
+        body("fullname.firstname")
+            
+            .notEmpty().withMessage("First name is required")
+            .bail()
             .isString().withMessage("First name must be a string")
+            .trim()
+            .bail()
             .isLength({min:3}).withMessage("First name must be at least 3 character"),
 
-        body("fullName.lastname")
+        body("fullname.lastname")
             .optional()
-            .trim()
+            
             .isString().withMessage("Last name must be a string")
+            .trim()
             .isLength({min:3}).withMessage("Last name must be at least 3 character"),
 
         body("email")
@@ -21,26 +26,32 @@ const registerCaptainValidation = () => {
             .isLength({min:6}).withMessage("Password must be atleast 6 character"),
 
         body("contact")
-            .trim()
             .notEmpty().withMessage("Contact number is required")
-            .isString().withMessage("Contact must be a string"),
+            .isString().withMessage("Contact must be a string")
+            .trim(),
 
         body("vehicle.color")
-            .trim()
+            
             .isString().withMessage("Color must be a string")
+            .trim()
+            .bail()
             .isLength({min:3}).withMessage("Color must be at least 3 character"),
 
         body("vehicle.plate")
-            .trim()
+            
             .isString().withMessage("Plate must be a string")
+            .trim()
+            .bail()
             .isLength({min:3}).withMessage("Plate must be at least 3 character"),
 
         body("vehicle.capacity")
             .isInt({min:1}).withMessage("Capacity must be a number"),
-            
+
         body("vehicle.vehicleType")
-            .trim()
+            
             .isString().withMessage("Vehicle type must be a string")
+            .trim()
+            .bail()
             .isIn(["car", "motorcycle", "auto"]).withMessage('Invalid vehicle type')
     ]        
 };
