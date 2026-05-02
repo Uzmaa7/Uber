32a2a41d9b2130de9ad549acdaa4ada579c96396
Uzmaa7 <uk9971khan@gmail.com>
2026-05-02 18:25:32 +0530

    Feat: Added initial UI with map and pickup and destination fields

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,14 +4,33 @@ import gsap from 'gsap';
 import axios from 'axios';
 import 'remixicon/fonts/remixicon.css'
 
+import LocationSearchPanel from '../components/LocationSearchPanel';
+import VehiclePanel from '../components/VehiclePanel';
+import ConfirmRide from '../components/ConfirmRide';
+
+
+import { useContext } from 'react';
+import {useUserContext} from '../context/UserContext'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-    const [pickup, setPickup] = useState('')
-    const [destination, setDestination] = useState('')
-    const [panelOpen, setPanelOpen] = useState(false)
+    const [ pickup, setPickup ] = useState('')
+    const [ destination, setDestination ] = useState('')
+    const [ panelOpen, setPanelOpen ] = useState(false)
 
+    const vehiclePanelRef = useRef(null)
+    const confirmRidePanelRef = useRef(null)
+   
+   
     const panelRef = useRef(null)
     const panelCloseRef = useRef(null)
+
+    const [ vehiclePanel, setVehiclePanel ] = useState(false)
+     const [ vehicleType, setVehicleType ] = useState(null)
+    const [ confirmRidePanel, setConfirmRidePanel ] = useState(false)
+
+
+    const navigate = useNavigate()
 
 
     const submitHandler = (e) => {
@@ -38,31 +57,53 @@ const Home = () => {
                 opacity: 0
             })
         }
-    }, [panelOpen])
+    }, [ panelOpen ])
+
+
+    useGSAP(function () {
+        if (vehiclePanel) {
+            gsap.to(vehiclePanelRef.current, {
+                transform: 'translateY(0)'
+            })
+        } else {
+            gsap.to(vehiclePanelRef.current, {
+                transform: 'translateY(100%)'
+            })
+        }
+    }, [ vehiclePanel ])
+
+    useGSAP(function () {
+        if (confirmRidePanel) {
+            gsap.to(confirmRidePanelRef.current, {
+                transform: 'translateY(0)'
+            })
+        } else {
+            gsap.to(confirmRidePanelRef.current, {
+                transform: 'translateY(100%)'
+            })
+        }
+    }, [ confirmRidePanel ])
 
 
     return (
         <div className='h-screen relative overflow-hidden'>
 
-                            {/* logo */}
             <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
-                            {/* map */}
             <div className='h-screen w-screen'>
-                <img className='w-full object-fill' src="https://i.sstatic.net/gtiI7.gif" alt="" />
+                {/* image for temporary use  */}
                 {/* <LiveTracking /> */}
-            </div>
 
+                <img className='w-full object-fill' src="https://i.sstatic.net/gtiI7.gif" alt="" />
+
+            </div>
             <div className=' flex flex-col justify-end h-screen absolute top-0 w-full'>
                 <div className='h-[30%] p-6 bg-white relative'>
-                            {/* down arrow  */}
                     <h5 ref={panelCloseRef} onClick={() => {
                         setPanelOpen(false)
                     }} className='absolute opacity-0 right-6 top-6 text-2xl'>
                         <i className="ri-arrow-down-wide-line"></i>
                     </h5>
-
                     <h4 className='text-2xl font-semibold'>Find a trip</h4>
-
                     <form className='relative py-3' onSubmit={(e) => {
                         submitHandler(e)
                     }}>
@@ -70,12 +111,10 @@ const Home = () => {
                         <input
                             onClick={() => {
                                 setPanelOpen(true)
-
+                               
                             }}
                             value={pickup}
-                            onChange={(e) => {
-                                setPickup(e.target.value)
-                            }}
+                            
                             className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full'
                             type="text"
                             placeholder='Add a pick-up location'
@@ -83,34 +122,44 @@ const Home = () => {
                         <input
                             onClick={() => {
                                 setPanelOpen(true)
-
+                               
                             }}
                             value={destination}
-                            onChange={(e) => {
-                                setDestination(e.target.value)
-                            }}
+                           
                             className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full  mt-3'
                             type="text"
                             placeholder='Enter your destination' />
                     </form>
-                    
                     <button
-                        // onClick={findTrip}
+                       
                         className='bg-black text-white px-4 py-2 rounded-lg mt-3 w-full'>
                         Find Trip
                     </button>
-
-                    <div ref={panelRef} className='bg-white h-0'>
-                        {/* <LocationSearchPanel/> */}
-                        <div className='p-5'>
-                            {/* Testing content to see if it works */}
-                            <p className='text-gray-500'>Suggested locations will appear here...</p>
-                        </div>
-                    </div>
-
                 </div>
-
+                <div ref={panelRef} className='bg-white h-0'>
+                    <LocationSearchPanel
+                        
+                        setPanelOpen={setPanelOpen}
+                        setVehiclePanel={setVehiclePanel}
+                       
+                    />
+                </div>
             </div>
+            <div ref={vehiclePanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+                <VehiclePanel
+                    selectVehicle={setVehicleType}
+                    setConfirmRidePanel={setConfirmRidePanel} setVehiclePanel={setVehiclePanel} />
+            </div>
+            <div ref={confirmRidePanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-6 pt-12'>
+                <ConfirmRide
+                   
+                 
+                   
+
+                    setConfirmRidePanel={setConfirmRidePanel}  />
+            </div>
+            
+            
         </div>
     )
 }
