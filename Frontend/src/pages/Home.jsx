d88a23d681b60b204048d19ef7a4b6eaf5e1a926
Uzmaa7 <uk9971khan@gmail.com>
2026-05-02 14:29:10 +0530

    feat: add Home page layout and integrated search panel animations

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,118 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from 'react'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import axios from 'axios';
+import 'remixicon/fonts/remixicon.css'
 
-function Home(){
-    return(
-        <div>
-            Home
-        </div>    
+
+const Home = () => {
+    const [pickup, setPickup] = useState('')
+    const [destination, setDestination] = useState('')
+    const [panelOpen, setPanelOpen] = useState(false)
+
+    const panelRef = useRef(null)
+    const panelCloseRef = useRef(null)
+
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+    }
+
+    useGSAP(function () {
+        if (panelOpen) {
+            gsap.to(panelRef.current, {
+                height: '70%',
+                padding: 24
+                // opacity:1
+            })
+            gsap.to(panelCloseRef.current, {
+                opacity: 1
+            })
+        } else {
+            gsap.to(panelRef.current, {
+                height: '0%',
+                padding: 0
+                // opacity:0
+            })
+            gsap.to(panelCloseRef.current, {
+                opacity: 0
+            })
+        }
+    }, [panelOpen])
+
+
+    return (
+        <div className='h-screen relative overflow-hidden'>
+
+                            {/* logo */}
+            <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+                            {/* map */}
+            <div className='h-screen w-screen'>
+                <img className='w-full object-fill' src="https://i.sstatic.net/gtiI7.gif" alt="" />
+                {/* <LiveTracking /> */}
+            </div>
+
+            <div className=' flex flex-col justify-end h-screen absolute top-0 w-full'>
+                <div className='h-[30%] p-6 bg-white relative'>
+                            {/* down arrow  */}
+                    <h5 ref={panelCloseRef} onClick={() => {
+                        setPanelOpen(false)
+                    }} className='absolute opacity-0 right-6 top-6 text-2xl'>
+                        <i className="ri-arrow-down-wide-line"></i>
+                    </h5>
+
+                    <h4 className='text-2xl font-semibold'>Find a trip</h4>
+
+                    <form className='relative py-3' onSubmit={(e) => {
+                        submitHandler(e)
+                    }}>
+                        <div className="line absolute h-16 w-1 top-[50%] -translate-y-1/2 left-5 bg-gray-700 rounded-full"></div>
+                        <input
+                            onClick={() => {
+                                setPanelOpen(true)
+
+                            }}
+                            value={pickup}
+                            onChange={(e) => {
+                                setPickup(e.target.value)
+                            }}
+                            className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full'
+                            type="text"
+                            placeholder='Add a pick-up location'
+                        />
+                        <input
+                            onClick={() => {
+                                setPanelOpen(true)
+
+                            }}
+                            value={destination}
+                            onChange={(e) => {
+                                setDestination(e.target.value)
+                            }}
+                            className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full  mt-3'
+                            type="text"
+                            placeholder='Enter your destination' />
+                    </form>
+                    
+                    <button
+                        // onClick={findTrip}
+                        className='bg-black text-white px-4 py-2 rounded-lg mt-3 w-full'>
+                        Find Trip
+                    </button>
+
+                    <div ref={panelRef} className='bg-white h-0'>
+                        {/* <LocationSearchPanel/> */}
+                        <div className='p-5'>
+                            {/* Testing content to see if it works */}
+                            <p className='text-gray-500'>Suggested locations will appear here...</p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
     )
 }
 
-export default Home;
+export default Home
