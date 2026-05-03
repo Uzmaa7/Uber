86f34d0e617604a1dfbee66267a48b66e6a5ff02
Uzmaa7 <uk9971khan@gmail.com>
2026-05-03 15:23:24 +0530

    Feat:Added ConfirmRidePopUp component for captain ride validation

--- Frontend/src/components/ConfirmRidePopUp.jsx
+++ Frontend/src/components/ConfirmRidePopUp.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
-const RidePopUp = (props) => {
+
+const ConfirmRidePopUp = (props) => {
+
+
     return (
         <div>
-
             <h5 className='p-1 text-center w-[93%] absolute top-0' onClick={() => {
                 props.setRidePopupPanel(false)
-            }}>
-                <i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i>
-            </h5>
-
-            <h3 className='text-2xl font-semibold mb-5'>New Ride Available!</h3>
-            <div className='flex items-center justify-between p-3 bg-yellow-400 rounded-lg mt-4'>
+            }}><i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
+            <h3 className='text-2xl font-semibold mb-5'>Confirm this ride to Start</h3>
+            <div className='flex items-center justify-between p-3 border-2 border-yellow-400 rounded-lg mt-4'>
                 <div className='flex items-center gap-3 '>
                     <img className='h-12 rounded-full object-cover w-12' src="https://i.pinimg.com/236x/af/26/28/af26280b0ca305be47df0b799ed1b12b.jpg" alt="" />
-                    <h2 className='text-lg font-medium'>user.fullname</h2>
+                    <h2 className='text-lg font-medium capitalize'>ride.user.firstname</h2>
                 </div>
                 <h5 className='text-lg font-semibold'>2.2 KM</h5>
             </div>
@@ -42,24 +43,23 @@ const RidePopUp = (props) => {
                         </div>
                     </div>
                 </div>
-                <div className='mt-5 w-full '>
 
-                    <button onClick={() => {
-                       props.setConfirmRidePopupPanel(true)
-                      
+                <div className='mt-6 w-full'>
+                    <form >
+                        
 
-                    }} className=' bg-green-600 w-full text-white font-semibold p-2 px-10 rounded-lg'>Accept</button>
+                        <button className='w-full mt-5 text-lg flex justify-center bg-green-600 text-white font-semibold p-3 rounded-lg'>Confirm</button>
+                        <button onClick={() => {
+                            props.setConfirmRidePopupPanel(false)
+                            props.setRidePopupPanel(false)
 
-                    <button onClick={() => {
-                        props.setRidePopupPanel(false)
+                        }} className='w-full mt-2 bg-red-600 text-lg text-white font-semibold p-3 rounded-lg'>Cancel</button>
 
-                    }} className='mt-2 w-full bg-gray-300 text-gray-700 font-semibold p-2 px-10 rounded-lg'>Ignore</button>
-
-
+                    </form>
                 </div>
             </div>
         </div>
     )
 }
 
-export default RidePopUp
+export default ConfirmRidePopUp
--- Frontend/src/pages/CaptainHome.jsx
+++ Frontend/src/pages/CaptainHome.jsx
@@ -8,11 +8,15 @@ import { useEffect, useContext } from 'react'
 
 import CaptainDetails from '../components/CaptainDetails.jsx'
 import RidePopUp from '../components/RidePopUp.jsx'
+import ConfirmRidePopUp from '../components/ConfirmRidePopUp.jsx';
 
 
 const CaptainHome = () => {
-    const [ ridePopupPanel, setRidePopupPanel ] = useState(flase);
+    const [ridePopupPanel, setRidePopupPanel] = useState(true);
     const ridePopupPanelRef = useRef(null)
+
+    const [confirmRidePopupPanel, setConfirmRidePopupPanel] = useState(false)
+    const confirmRidePopupPanelRef = useRef(null)
 
 
 
@@ -26,11 +30,24 @@ const CaptainHome = () => {
                 transform: 'translateY(100%)'
             })
         }
-    }, [ ridePopupPanel ])
+    }, [ridePopupPanel])
+
+
+    useGSAP(function () {
+        if (confirmRidePopupPanel) {
+            gsap.to(confirmRidePopupPanelRef.current, {
+                transform: 'translateY(0)'
+            })
+        } else {
+            gsap.to(confirmRidePopupPanelRef.current, {
+                transform: 'translateY(100%)'
+            })
+        }
+    }, [confirmRidePopupPanel])
 
 
 
-        return (
+    return (
         <div className='h-screen'>
 
             <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
@@ -40,19 +57,26 @@ const CaptainHome = () => {
                 </Link>
             </div>
 
-             <div className='h-3/5'>
+            <div className='h-3/5'>
                 <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
 
             </div>
-            
+
             <div className='h-2/5 p-6'>
                 <CaptainDetails />
             </div>
 
-             <div ref={ridePopupPanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+            <div ref={ridePopupPanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
                 <RidePopUp
-                  setRidePopupPanel={setRidePopupPanel}  
+                    setRidePopupPanel={setRidePopupPanel}
+                     setConfirmRidePopupPanel={setConfirmRidePopupPanel}
                 />
+            </div>
+
+
+            <div ref={confirmRidePopupPanelRef} className='fixed w-full h-screen z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+                <ConfirmRidePopUp
+                    setConfirmRidePopupPanel={setConfirmRidePopupPanel} setRidePopupPanel={setRidePopupPanel} />
             </div>
 
 
