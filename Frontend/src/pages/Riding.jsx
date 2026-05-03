32052c604df18c2e1e61c10b1d61330fba4b231a
Uzmaa7 <uk9971khan@gmail.com>
2026-05-03 14:00:50 +0530

    feat: implement initial CaptainHome layout with CaptainDetails component integration

--- Frontend/src/pages/Riding.jsx
+++ Frontend/src/pages/Riding.jsx
@@ -7,6 +7,10 @@ function Riding() {
             <Link to='/home' className='fixed right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full'>
                 <i className="text-lg font-medium ri-home-5-line"></i>
             </Link>
+
+            <div className='h-1/2'>
+                <img className='h-full w-full object-cover' src="https://i.sstatic.net/gtiI7.gif" alt="" />
+            </div>
             
             <div className='h-1/2 p-4'>
                 <div className='flex items-center justify-between'>
