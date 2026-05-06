7e8a06d2280c74d103b3d2de27d110c49cda1abb
Uzmaa7 <uk9971khan@gmail.com>
2026-05-06 16:15:45 +0530

    feat: add real-time location suggestions from backend

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 
-const LocationSearchPanel = (props) => {
+const LocationSearchPanel = ({setPanelOpen, activeField, suggestions, setPickup,setDestination, setVehiclePanel }) => {
 
-    const locations = [
-        "24B, Near Kapoor's cafe",
-        "70A, Noida son market",
-        "yamuna 34 C-1 Block malhotra shop"
-    ]
+    // const locations = [
+    //     "24B, Near Kapoor's cafe",
+    //     "70A, Noida son market",
+    //     "yamuna 34 C-1 Block malhotra shop"
+    // ]
+
+
+    // sample array for location 
+    const handleSuggestionClick = (suggestion) => {
+        if(activeField === 'pickup'){
+            setPickup(suggestion)
+        }else{
+            setDestination(suggestion)
+        }
+        // console.log(suggestions)
+    }
 
     return (
         <div >
 
-            {
+            {/* {
                 locations.map(function (elem, idx) {
                     return <div key={idx} onClick={()=>{
                         props.setVehiclePanel(true)
@@ -21,6 +32,17 @@ const LocationSearchPanel = (props) => {
                         <h4 className="font-medium">{elem}</h4>
                     </div>
                 })
+            } */}
+
+
+            {
+                Array.isArray(suggestions) ? suggestions.map((elem, idx) => (
+                    <div key={idx} onClick={() => {handleSuggestionClick(elem.description)}}
+                    className='flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-2 justify-start'>
+                        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+                        <h4 className='font-medium'>{elem.description}</h4>
+                    </div>
+                )): <p className="p-2 text-gray-500">No suggestions available</p>
             }
 
 
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -17,8 +17,9 @@ import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
     const [pickup, setPickup] = useState('')
-    const [destination, setDestination] = useState('')
     const [panelOpen, setPanelOpen] = useState(false)
+    const [destination, setDestination] = useState('')
+    
 
     const vehiclePanelRef = useRef(null)
     const confirmRidePanelRef = useRef(null)
@@ -34,6 +35,11 @@ const Home = () => {
     const [confirmRidePanel, setConfirmRidePanel] = useState(false)
     const [vehicleFound, setVehicleFound] = useState(false)
     const [ waitingForDriver, setWaitingForDriver ] = useState(false)
+
+    const {user, setUser, authToken, setAuthToken, isAuthReady } = useUserContext()
+     const [activeField, setActiveField] = useState(null)
+     const [pickUpSuggestions, setPickUpSuggestions] = useState([])
+    const [destinationSuggestions, setDestinationSuggestions] = useState([])
 
     const navigate = useNavigate()
 
@@ -113,17 +119,63 @@ const Home = () => {
         }
     }, [ waitingForDriver ])
 
+
+     const handlePickUpChange = async (e) => {
+        setPickup(e.target.value)
+        try {
+            const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/maps/get-suggestions`, {
+                params : {
+                    input : e.target.value
+                },
+                headers : {
+                    Authorization : `Bearer ${authToken}`
+                },
+                withCredentials : true
+            })
+            // console.log("PickUp Suggestions => ", response.data.data)
+
+            setPickUpSuggestions(response.data.data.suggestions)
+        } catch (error) {
+            console.log('Error while loading pickUp suggestions')
+        }
+    }
+
+    const handleDestinationChange = async (e) => {
+        setDestination(e.target.value);
+        try {
+            const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/maps/get-suggestions`, {
+                params : {
+                    input : e.target.value
+                },
+                headers : {
+                    Authorization : `Bearer ${authToken}`
+                },
+                withCredentials : true
+            })
+
+            // console.log("Destination Suggestions => ", response.data.data)
+
+            setDestinationSuggestions(response.data.data.suggestions)
+        } catch (error) {
+            console.log('Error while loading Destination suggestions')
+        }
+    }
+
     return (
         <div className='h-screen relative overflow-hidden'>
-
+            {/* Logo */}
             <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
-            <div className='h-screen w-screen'>
-                {/* image for temporary use  */}
-                {/* <LiveTracking /> */}
 
+            {/* Map */}
+            <div className='h-screen w-screen'>
+
+                {/* image for temporary use  */}
                 <img className='w-full object-fill' src="https://i.sstatic.net/gtiI7.gif" alt="" />
 
+                {/* <LiveTracking /> */}
             </div>
+
+
             <div className=' flex flex-col justify-end h-screen absolute top-0 w-full'>
                 <div className='h-[30%] p-6 bg-white relative'>
                     <h5 ref={panelCloseRef} onClick={() => {
@@ -131,6 +183,7 @@ const Home = () => {
                     }} className='absolute opacity-0 right-6 top-6 text-2xl'>
                         <i className="ri-arrow-down-wide-line"></i>
                     </h5>
+
                     <h4 className='text-2xl font-semibold'>Find a trip</h4>
                     <form className='relative py-3' onSubmit={(e) => {
                         submitHandler(e)
@@ -139,8 +192,10 @@ const Home = () => {
                         <input
                             onClick={() => {
                                 setPanelOpen(true)
+                                setActiveField('pickup')
 
                             }}
+                            onChange = {(e) => handlePickUpChange(e)}
                             value={pickup}
 
                             className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full'
@@ -150,8 +205,11 @@ const Home = () => {
                         <input
                             onClick={() => {
                                 setPanelOpen(true)
+                                setActiveField('destination')
 
                             }}
+
+                            onChange = {(e) => {handleDestinationChange(e)}}
                             value={destination}
 
                             className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full  mt-3'
@@ -164,10 +222,18 @@ const Home = () => {
                         Find Trip
                     </button>
                 </div>
+
                 <div ref={panelRef} className='bg-white h-0'>
                     <LocationSearchPanel
 
                         setPanelOpen={setPanelOpen}
+                        activeField = {activeField}
+                        suggestions = {activeField === 'pickup' ? pickUpSuggestions : destinationSuggestions}
+                        
+                        setPickup = {setPickup}
+                        setDestination = {setDestination}
+
+
                         setVehiclePanel={setVehiclePanel}
                         vehiclePanel={vehiclePanel}
 
